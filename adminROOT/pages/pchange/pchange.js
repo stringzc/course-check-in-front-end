2a8d7890a09cf1f8b156b55c2596cf69c71a2f29
郑超 <1391293227@qq.com>
2023-08-15 17:55:54 +0800

    基本写完管理页面的前端

--- adminROOT/pages/pchange/pchange.js
+++ adminROOT/pages/pchange/pchange.js
@@ -11,83 +11,199 @@ Page({
     users:50,
     Hots:[
       {
+        id:"01",
         name:"c+++++",
         color:"red",
         width:"50%",
         url:"/static/hf1.png"
       },
       {
+        id:"10",
         name:"c+++++",
-        color:"#F3D853",
+        color:"IndianRed",
         width:"60%",
+        url:"/static/hf2.png"
+      }
+    ],
+    clslist:[
+      {
+        id:"01",
+        name:"c+++++",
+        color:"red",
         url:"/static/hf2.png"
       },
       {
+        id:"10",
         name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-      {
-        name:"c+++++",
-        color:"#F16D2F",
-        width:"70%",
-        url:"/static/hf3.png"
-      },
-
-
+        color:"IndianRed",
+        url:"/static/hf2.png"
+      }
     ],
-    kuan:500,
-    color:"red"
+    datelist:[
+      {
+        id:"10",
+        name:"c+++++",
+        checked:false
+      }
+    ],
+    dllist:[
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+      {
+        name:"zczczc",
+        times:"2023.08.06 14:22"
+      },
+    ],
+    bdlist:[
+    ],
+    qdlist:[
+      {name:"zczczc",
+       class:"c+++++",
+       times:"2023.08.06 14:22"
+    }
+    ],
+    thisi:-1,
+    thiscolor:"",
+    checks:false,
+    array: ['一个月前', '三个月前', '一年前', '全部'],
+    index:0,
+    tab34checks:false,
+    tab33checks:false,
+    tab32checks:false,
+    tab31checks:false
 
   },
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  clickmes(e){
+    this.setData({
+      checks: !this.data.checks
+    })
+    var checks = this.data.checks
+    var lists  = this.data.datelist
+    for(var i = 0; i < lists.length; i ++ )
+    {
+      lists[i].checked = checks
+    }
+    this.setData({
+        datelist:lists
+      })
+  },
   onTabClick(e) {
-    let id = e.currentTarget.id;
+    let id = e.currentTarget.id
+    let that = this
     this.setData({
       tabIndex: id,
     })
+    
+    // if(id == 0)
+    // {
+
+    // }
+    // else if(id == 1)
+    // {
+    //   getApp().helper({
+    //     url: getApp().globalData.urlpath + '/getcls',
+    //     data: {
+    //       ID:"wx"
+    //     },
+    //     success(res) {
+    //       that.setData({
+    //         clslist:res.data.clslist
+    //       })
+    //     },
+    //     fail(res) {
+    //       wx.showToast({
+    //         title: '连接服务器失败',
+    //         image: '/static/error.png',
+    //         duration: 1500
+    //       });
+    //     }
+    //   });
+    // }
+    // else
+    // {
+
+    // }
+  },
+  anxia(e){
+    var id = e.currentTarget.dataset.id
+    var lists = this.data.clslist
+    var lasti = this.data.thisi
+    var lastcolor = this.data.thiscolor
+    var that = this
+    if(lastcolor.length!=0 && lasti != -1)
+    {
+      lists[lasti].color = lastcolor
+      that.setData({
+        clslist:lists
+      })
+    }
+    for(var i = 0; i < lists.length; i ++ )
+    {
+      if(lists[i].id == id)
+      {
+        var newcolor = lists[i].color
+        lists[i].color = "Gainsboro"
+        that.setData({
+          clslist:lists,
+          thisi:i,
+          thiscolor:newcolor
+        })
+      }
+    }
+    getApp().helper({
+      url: getApp().globalData.urlpath + '/getdate',
+      data: {
+        id: id,
+        ID:"wx"
+      },
+      success(res) {
+        that.setData({
+          datelist:res.data.datelist
+        })
+      },
+      fail(res) {
+        wx.showToast({
+          title: '连接服务器失败',
+          image: '/static/error.png',
+          duration: 1500
+        });
+      }
+    });
   },
   /**
    * 生命周期函数--监听页面加载
